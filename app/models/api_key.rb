796d16fe49b17f9ef8097eafef0ea2266612aa21
Hugh Davenport <hugh.davenport@powershop.co.nz>
2016-04-21 13:35:33 +1200

    Generate keys

--- app/models/api_key.rb
+++ app/models/api_key.rb
@@ -1,3 +1,10 @@
 class ApiKey < ActiveRecord::Base
   belongs_to :user
+
+  after_initialize :generate_keys
+
+  def generate_keys
+    self.secret ||= ApiAuth.generate_secret_key
+    self.public ||= SecureRandom.base64
+  end
 end
